748e60e5d8bb9a5936de2b55bee050f063489937
Nam HG <onep5286@gamil.com>
2019-12-08 03:41:23 +0900

    fine

--- crawling_test.py
+++ crawling_test.py
@@ -1,9 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://comic.naver.com/webtoon/weekdayList.nhn?week=mon'
+url = 'https://comic.naver.com/webtoon/weekdayList.nhn?week=sun'
 html = requests.get(url)
 #print(html.text)
 soup = BeautifulSoup(html.text, 'html.parser')
-print(soup.select_one('#content > div.list_area.daily_img > ul > li:nth-child(1) > dl > dt > a'))
-#print(soup.select('div.mon'))
+
+#print(soup.select_one('#content > div.list_area.daily_img > ul > li:nth-child(1) > dl > dt > a'))
+
+title_list = soup.select('ul.img_list')
+for i in title_list:
+    print(i.select_one('dt > a').text.strip())
