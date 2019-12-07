ad0048de2a6a79dc016620b38290c72494a8820f
Nam HG <onep5286@gamil.com>
2019-12-07 18:31:33 +0900

    search god of tower

--- crawling_test.py
+++ crawling_test.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://comic.naver.com/webtoon/list.nhn?titleId=183559&weekday=mon'
+url = 'https://comic.naver.com/webtoon/weekday.nhn'
 html = requests.get(url)
-print(html.text)
+#print(html.text)
+soup = BeautifulSoup(html.text, 'html.parser')
+print(soup.select_one('#content > div.list_area.daily_all > div:nth-child(1) > div > ul > li:nth-child(1) > a'))
