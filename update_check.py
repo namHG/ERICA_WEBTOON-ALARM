bde73078876ec05adb0624745ae00e6e13a57e8c
Nam HG <onep5286@gamil.com>
2019-12-08 03:41:43 +0900

    error

--- update_check.py
+++ update_check.py
@@ -6,6 +6,8 @@ html = requests.get(url)
 soup = BeautifulSoup(html.text, 'html.parser')
 update = soup.select_one('em.ico_updt')
 if(update):
-	print("Update!")
+    update = update.find_parent('div', class_= 'list_area daily_img')
+    title = update.select_one('ul.img_list > li > dt > a').text.strip()
+    print(title + " Update!")
 else:
-	print("Not update.")
+    print("Not update.")
