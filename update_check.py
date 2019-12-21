0e8a6159c78e2f40338a5104625d570bf0cb71ca
Nam HG <onep5286@gamil.com>
2019-12-21 17:47:54 +0900

    fri, sat error

--- update_check.py
+++ update_check.py
@@ -1,15 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://comic.naver.com/webtoon/weekdayList.nhn?week=sat'
+url = 'https://comic.naver.com/webtoon/weekdayList.nhn?week=sun'
 html = requests.get(url)
 soup = BeautifulSoup(html.text, 'html.parser')
 update = soup.select_one('em.ico_updt')
+print(update)
 if(update):
     update = update.find_parent('div', class_= 'list_area daily_img')
     title = update.select_one('ul > li:nth-child(1) > dl > dt > a').text.strip()
     print(title + " is updated!\n")
 else:
     update = update.find_parent('div', class_= 'list_area daily_img')
-    title = update.select_one('ul.img_list > dl > dt > a').text.strip()
+    title = update.select_one('ul > li:nth-child(1) > dl > dt > a').text.strip()
     print(title + "is not updated.\n")
