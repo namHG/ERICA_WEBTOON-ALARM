361410ab598d4c839ecbeeb0ef1ddcec30829177
Nam HG <onep5286@gamil.com>
2019-12-21 18:37:53 +0900

    searching title

--- crawling.py
+++ crawling.py
@@ -6,4 +6,4 @@ html = requests.get(url)
 soup = BeautifulSoup(html.text, 'html.parser')
 title_list = soup.select('ul.img_list')
 for i in title_list:
-    print(i.select_one('dt > a').text.strip())
+    print(i.select_one('ul > li:nth-child(2) > dl > dt > a').text.strip())
