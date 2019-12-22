46fd7dfbe46646385294bbcfc13c08e177c6fa21
Nam HG <onep5286@gamil.com>
2019-12-22 19:33:52 +0900

    update check

--- update_check.py
+++ update_check.py
@@ -7,8 +7,8 @@ soup = BeautifulSoup(html.text, 'html.parser')
 update = soup.select_one('em.ico_updt')
 if(update):
     update = update.find_parent('div', class_= 'list_area daily_img')
-    title = update.select_one('li:nth-child(3) > dl > dt > a').text.strip()
-    update2 = update.select_one('li:nth-child(3) > div > a > em')
+    title = update.select_one('li:nth-child(2) > dl > dt > a').text.strip()
+    update2 = update.select_one('li:nth-child(2) > div > a > em')
     if(update2):
         print(title + " is updated!\n")
     else:
