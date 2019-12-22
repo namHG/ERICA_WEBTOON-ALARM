0983b3527949a95edc21a84e15c6fc128210662e
Nam HG <onep5286@gamil.com>
2019-12-22 18:09:01 +0900

    mon tue wed error

--- update_check.py
+++ update_check.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://comic.naver.com/webtoon/weekdayList.nhn?week=mon'
+url = 'https://comic.naver.com/webtoon/weekdayList.nhn?week=sun'
 html = requests.get(url)
 soup = BeautifulSoup(html.text, 'html.parser')
 update = soup.select_one('em.ico_updt')
