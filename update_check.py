4af94b944f4bc93b5684ab824304c5600dba8baf
Nam HG <onep5286@gamil.com>
2019-12-22 19:26:04 +0900

    update check

--- update_check.py
+++ update_check.py
@@ -1,14 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://comic.naver.com/webtoon/weekdayList.nhn?week=tue'
+url = 'https://comic.naver.com/webtoon/weekdayList.nhn?week=sun'
 html = requests.get(url)
 soup = BeautifulSoup(html.text, 'html.parser')
 update = soup.select_one('em.ico_updt')
 if(update):
     update = update.find_parent('div', class_= 'list_area daily_img')
-    title = update.select_one('li:nth-child(1) > dl > dt > a').text.strip()
-    update2 = update.select_one('li:nth-child(1) > div > a > em')
+    title = update.select_one('li:nth-child(3) > dl > dt > a').text.strip()
+    update2 = update.select_one('li:nth-child(3) > div > a > em')
     if(update2):
         print(title + " is updated!\n")
     else:
